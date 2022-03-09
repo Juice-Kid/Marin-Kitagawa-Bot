2a28c223f712efd922a9b3350549ed15af38d601
CHEY <97988840+Chey-san@users.noreply.github.com>
2022-03-09 06:33:55 +0530

    Update Chey.ts

--- src/commands/Moderation/Chey.ts
+++ src/commands/Moderation/Chey.ts
@@ -29,6 +29,24 @@ export default class Command extends BaseCommand {
 				mimetype: Mimetype.gif,
 				caption: `Thx for using the cmd check your xp & don't forget to follow me:🔻.
 
+run = async (M: ISimplifiedMessage): Promise<void> => {
+    const user = M.sender.jid;
+    const time = 86400000;
+    const cd = await (await this.client.getUser(user)).lastDaily;
+    if (time - (Date.now() - cd) > 0) {
+      const timeLeft = ms(time - (Date.now() - cd));
+      return void M.reply(
+        `🟨 You claimed your daily gold recently. Claim again in *${timeLeft.hours} hour(s), ${timeLeft.minutes} minute(s), ${timeLeft.seconds} second(s)*`
+      );
+    }
+    await this.client.addGold(user, 1000);
+    await this.client.DB.user.updateOne(
+      { jid: user },
+      { $set: { lastDaily: Date.now() } }
+    );
+    return void M.reply(`🎉 *5000 gold* has been added to your wallet.`);
+
+
 ⭕𝙂𝙞𝙩𝙝𝙪𝙗;
 https://github.com/Chey-san
 📮𝙄𝙣𝙨𝙩𝙖𝙜𝙧𝙖𝙢;
